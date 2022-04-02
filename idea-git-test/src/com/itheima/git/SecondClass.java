23132133a095a2615462e6e39e426de07199d42f
zhangsan <zhangsan@itcast.cn>
2022-04-02 16:31:16 +0800

    修改SecondClass

---
+++ idea-git-test/src/com/itheima/git/SecondClass.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+public class SecondClass {
+
+    public void test(){
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("你好");
+    }
+}
