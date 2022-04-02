46f4df0c286fc605473dc67e32a16da5b6f84de3
zhangsan <zhangsan@itcast.cn>
2022-04-02 10:52:55 +0800

    修改FirstClass

--- idea-git-test/src/com/itheima/git/FisrtClass.java
+++ idea-git-test/src/com/itheima/git/FisrtClass.java
@@ -5,4 +5,8 @@ public class FisrtClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
     }
+
+    public void test(){
+        int i=0;
+    }
 }
